616d682ee03505fe3f4f033b79896f266e70639b
Wladislaw <infinity89@gmail.com>
2011-12-01 10:04:17 +0200

    At last fix issue with building this fucking tree

--- hell/catalog/templatetags/catalog_tags.py
+++ hell/catalog/templatetags/catalog_tags.py
@@ -15,13 +15,18 @@ def left_menu(current_category=7):
     print "E V R I K A !"
     #cats = Catalog.objects.all()
     g = Catalog.objects.get(id=current_category)
-    ipath = drilldown_tree_for_node(par)
+    ipath = drilldown_tree_for_node(g)
     path = []
     others = {}
+    allcats = []
 
-    for p in ipars:
+    for p in ipath:
         path.append(p)
-        others[p] = p.get_children()
+
+    # some part of next two cicles need to be moved to recursive function. but what part exactly???
+    res = rec_call(path, path[0])
+    print g
+    print res
 
     return {'node':g, 'path':path, 'others':others}
 
@@ -30,3 +35,11 @@ def left_menu(current_category=7):
     # 1 get path
     # 2 get all children from path
     # 3 get all from root level its useless coz all children from path already include those also.
+
+def rec_call(path, parent):
+    res = []
+    for p in parent.get_children():
+        res.append(p)
+        if p in path:
+            res.extend(rec_call(path, p))
+    return res
