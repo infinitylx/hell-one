713c364a0a823aad08df18f639454394328f8ff6
Wladislaw <infinity89@gmail.com>
2011-12-03 13:50:59 +0200

    Got it work in my way

--- hell/catalog/templatetags/catalog_tags.py
+++ hell/catalog/templatetags/catalog_tags.py
@@ -24,22 +24,39 @@ def left_menu(current_category=7):
         path.append(p)
 
     # some part of next two cicles need to be moved to recursive function. but what part exactly???
-    res = rec_call(path, path[0])
+    res = rec_call(path, path[0], 0)
     print g
     print res
 
-    return {'node':g, 'path':path, 'others':others}
+    return {'node':g, 'path':path, 'others':res[0]}
 
-    # Todo:
-    # 0 get given object
-    # 1 get path
-    # 2 get all children from path
-    # 3 get all from root level its useless coz all children from path already include those also.
-
-def rec_call(path, parent):
+def rec_call(path, parent, counts):
+    """ 0 close list
+    1 open list
+    2 nothing
+    """
     res = []
+    b = False
     for p in parent.get_children():
-        res.append(p)
+
+
         if p in path:
-            res.extend(rec_call(path, p))
-    return res
+            res.append([p, 1])
+            counts += 1
+            r = rec_call(path, p, counts)
+            b = True
+            if len(r[0]) == 1:
+                res.extend([r[0]])
+            else:
+                res.extend(r[0])
+        else:
+            if b:
+                res.append([p, 0])
+            else:
+                res.append([p, 2])
+            b = False
+
+    #if len(res) == 0:
+    #    res = [None, 0]
+
+    return (res, counts-1)
